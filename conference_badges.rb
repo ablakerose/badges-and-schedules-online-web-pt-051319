e923c993c5d12f00c823e382f9f27d91c33faa69
Blake Rose <bdecktor@gmail.com>
2019-05-21 21:38:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.map{|name| badgemaker(name)}
+  attendees.map{|name| badge_maker(name)}
 end
 
 def assign_rooms(array_names)
